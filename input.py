b4f9259492a1894f84c0d2d2cf02b5020b27cdd9
MaxleyCv <maxim.lish@gmail.com>
2020-10-06 23:00:48 +0300

    added input from csv

---
+++ input.py
@@ -0,0 +1,15 @@
+import csv
+import utils
+import printer
+
+
+def get_printers_from_file():
+    csv_info_file = open(utils.INPUT_FILE, newline='')
+    reader = csv.reader(csv_info_file)
+    list_of_printers = []
+    for row in reader:
+        new_printer = printer.Printer(name=row[0], printing_speed=row[1], price_in_uah=row[2])
+        list_of_printers.append(new_printer)
+    return list_of_printers
+
+
